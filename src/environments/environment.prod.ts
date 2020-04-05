78a5cb19c9e740a1febcbdeeba96fe65f2ec87ac
POCSCRUM2 <avikdey201@gmail.com>
2020-04-05 18:35:49 +0530

    Environment files changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrls: {
+    local_dev: 'http://localhost:12001'
+  },
+  api_endpoints: {
+     getUserDetails: '/devops/authentication',
+     postUserDetails: '/devops/save',
+     submitRepoDetails: '/devops/submit'
+   
+  }
 };
