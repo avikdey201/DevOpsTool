01cf06d175985ca231a31ea2315d25a8b0a64d74
POCSCRUM2 <avikdey201@gmail.com>
2020-04-08 23:59:55 +0530

    Prod url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrls: {
-    local_dev: 'http://localhost:12001'
+    local_dev: 'http://172.16.85.61:8080/AppWeb'
   },
   api_endpoints: {
      getUserDetails: '/devops/authentication',
